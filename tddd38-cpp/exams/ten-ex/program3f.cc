d922143d7183b095557e92d36d653bd7c287f606
AxelGard <axel.gard@protonmail.com>
2021-12-27 16:53:17 +0100

    worked on tddd38 cpp exam

---
+++ tddd38-cpp/exams/ten-ex/program3f.cc
@@ -0,0 +1,66 @@
+#include <type_traits>
+#include <cassert>
+#include <string>
+
+#include <iostream>
+
+template <typename T1, typename T2>
+struct common_type
+{
+  using type = std::remove_reference_t<decltype(true ? std::declval<T1>() : std::declval<T2>())>;
+};
+
+template <typename T>
+struct common_type<T, void>
+{
+  using type = void;
+};
+
+template <typename T>
+struct common_type<void, T>
+{
+  using type = void;
+};
+
+template <>
+struct common_type<void, void>
+{
+  using type = void;
+};
+
+int main()
+{
+  // is_same_v is true if both types in the template-argument list are the same
+  // and false otherwise.
+  assert((std::is_same_v<
+          common_type<int, int>::type,
+          int>));
+  
+  assert((std::is_same_v<
+          common_type<int const, int>::type,
+          int const>));
+  
+  assert((std::is_same_v<
+          common_type<int, double>::type,
+          double>));
+  
+  assert((std::is_same_v<
+          common_type<double, int>::type,
+          double>));
+  
+  assert((std::is_same_v<
+          common_type<std::string, char const*>::type,
+          std::string>));
+
+  assert((std::is_same_v<
+          common_type<void, int>::type,
+          void>));
+
+  assert((std::is_same_v<
+          common_type<double, void>::type,
+          void>));
+
+  assert((std::is_same_v<
+          common_type<void, void>::type,
+          void>));
+}
--- tddd38-cpp/exams/ten-ex/program5.cc
+++ tddd38-cpp/exams/ten-ex/program5.cc
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <vector>
 #include <numeric>
+#include <algorithm>
 
 
 using namespace std;
@@ -43,14 +44,8 @@ using namespace std;
 int rotate(vector<int> & denominators, int k)
 {
   // move all elements in the range [0, k] to the end of the vector
-  for (int i{0}; i < k; ++i)
-  {
-    // move each element into position by swapping them to the end
-    for (int j{0}; j + 1 < denominators.size() - i; ++j)
-    {
-      swap(denominators[j], denominators[j+1]);
-    }
-  }
+// move each element into position by swapping them to the end
+  rotate(denominators, k);
 
   // return how far in the overlap ends
   return denominators.size() - k;
@@ -58,82 +53,48 @@ int rotate(vector<int> & denominators, int k)
 
 int main()
 {
-    int n, k;
-    std::cin >> n >> k;
+  int n, k;
+  std::cin >> n >> k;
 
-    // store a vector of all factors in the numerator
-    vector<int> factors(n - k, 0);
+  // store a vector of all factors in the numerator
+  vector<int> factors(n - k, 0);
 
-    // store a vector of all divisors in the denominator
-    vector<int> denominators(n - k, 0);
+  // store a vector of all divisors in the denominator
+  vector<int> denominators(n - k, 0);
 
-    iota(begin(factors), end(factors), k + 1);
+  // fill factors with k+1, k+2, ..., n-1, n
+  iota(begin(factors), end(factors), k + 1);
 
-    iota(begin(denominators), end(denominators), 1);
-    
-    if (denominators.size() >= k){
-        auto dist{rotate(denominators, k)};
-        factors.erase(begin(factors), next(begin(factors), dist));
-        denominators.erase(begin(denominators), next(begin(denominators), dist));
-    }
+  // fill denominators with 1, 2, ..., n-k-1, n-k
+  iota(begin(denominators), end(denominators), 1);
 
+  // if there is overlap between factors and denominators we remove it
+  if (denominators.size() >= k)
+  {
+    // restructure denominators so that the overlap is in the
+    // beginning of the vector and retrieve how many elements there
+    // where in the overlap
+    auto dist{rotate(denominators, k)};
 
-    // fill factors with k+1, k+2, ..., n-1, n
-    int counter{k + 1};
-    for (int i{0}; i < factors.size(); ++i)
-    {
-        factors[i] = counter++;
-    }
+    // remove the overlap
+    factors.erase(begin(factors), next(begin(factors), dist));
+    denominators.erase(begin(denominators), next(begin(denominators), dist));
+  }
 
-    // fill denominators with 1, 2, ..., n-k-1, n-k
-    counter = 1;
-    for (int i{0}; i < denominators.size(); ++i)
-    {
-        denominators[i] = counter++;
-    }
+  // for each denominator we want to divided it with a factor in factors
+  transform(begin(denominators), end(denominators), begin(denominators), 
+    [&factors](int den){
+      // find a factor that is divisible by the current denominator
+      auto it{find_if(begin(factors), end(factors), 
+          [den](int num){return num % den == 0;}
+        )};
+      *it /= den;  // divide the found factor
+      return 1;
+  });
 
-    // if there is overlap between factors and denominators we remove it
-    if (denominators.size() >= k)
-    {
-        // restructure denominators so that the overlap is in the
-        // beginning of the vector and retrieve how many elements there
-        // where in the overlap
-        auto dist{rotate(denominators, k)};
+  // multiply all factors together
+  int result{accumulate(begin(factors), end(factors),1, std::multiplies<int>{})};
 
-        // remove the overlap
-        for (int i{0}; i < dist; ++i)
-        {
-        factors.erase(factors.begin());
-        denominators.erase(denominators.begin());
-        }
-    }
-
-    // for each denominator we want to divided it with a factor in factors
-    for (int i{0}; i < denominators.size(); ++i)
-    {
-
-        // find a factor that is divisible by the current denominator
-        int j{0};
-        for (; j < factors.size(); ++j)
-        {
-            if (factors[j] % denominators[i] == 0)
-            {
-                break;
-            }
-        }
-        
-        // divide the found factor
-        factors[j] /= denominators[i];
-        denominators[i] = 1;
-    }
-
-    // multiply all factors together
-    int result{1};
-    for (int i{0}; i < factors.size(); ++i)
-    {
-        result *= factors[i];
-    }
-
-    // print the result
-    cout << result << endl;
+  // print the result
+  cout << result << endl;
 }
