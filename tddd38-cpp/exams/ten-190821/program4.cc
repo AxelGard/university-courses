e4b72eace804619959ac5cc833a2f9b032c759a1
Axel Gard <axel.gard@protonmail.com>
2022-03-16 19:04:27 +0100

    re did sfl exe

--- tddd38-cpp/exams/ten-190821/program4.cc
+++ tddd38-cpp/exams/ten-190821/program4.cc
@@ -6,8 +6,9 @@
 #include <fstream>
 #include <iterator>
 
-
 using namespace std;
+
+
 /* Testcase:
 
 achieve my advise -> put my tell
@@ -17,32 +18,27 @@ I consider a microscopic cloak to be irritable -> I think a little hide to be mo
 
  */
 
-int main() {
+int main()
+{
     // 1. open SYNONYMS
-    ifstream ifs {"SYNONYMS.txt"};
-    map<string, string> synonyms;
-    
+    ifstream ifs{"SYNONYMS.txt"};
     // 2. read the synonyms into the synonyms map
-    transform(istream_iterator<string>{ifs}, istream_iterator<string>{}, 
-        inserter(synonyms, begin(synonyms)), 
-        [](string const& line){
-            auto it{find(begin(line), end(line), '=')};
-            return make_pair(string{begin(line), it}, string{next(it), end(line)});
-        });
-    
+    map<string, string> synonyms{};
+    transform(istream_iterator<string>{ifs}, istream_iterator<string>{}, inserter(synonyms, begin(synonyms)), 
+        [](string const& row){
+            auto it{find(begin(row), end(row),'=')};
+            return make_pair(string{begin(row), it}, string{next(it), end(row)});
+    });
     // 3. read text from cin into the vector words, replacing each
-    //    possible word, leaving the rest as-is
     vector<string> words{};
-    transform(istream_iterator<string>{cin}, istream_iterator<string>{},
-        back_inserter(words), [&synonyms](string const& word){
-            auto it{synonyms.find(word)};
-            if (it != end(synonyms))
-                return it->second;
-            return word;
-        });
-
+    //    possible word, leaving the rest as-is
+    transform(istream_iterator<string>{cin}, istream_iterator<string>{}, back_inserter(words),
+            [&synonyms](string const& word){
+                auto it{synonyms.find(word)};
+                if (it != end(synonyms)) return it->second;
+                return word;
+            });
     // 4. print the text stored in words
-    copy(begin(words), end(words), ostream_iterator<string>{cout, " "});
+    copy(words.begin(), words.end(), ostream_iterator<string>{cout, " "});
     cout << endl;
-
 }
